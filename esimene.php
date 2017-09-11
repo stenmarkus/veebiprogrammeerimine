b5072e3366fdd0f5a257369c092a240445729fff
Sten Markus Laht <stenmarkus@usersnoreply.github.com>
2017-09-11 15:34:01 +0300

    muutujad ja if

--- esimene.php
+++ esimene.php
@@ -1,3 +1,23 @@
+<?php
+	//muutujad
+	$myName = "Sten Markus";
+	$myFamilyName = "Laht";
+	
+	$hourNow = date("H");
+	//echo $hournow;
+	//võrdlen kellaaega ja annan hinnangu, mis päeva osaga on tegemist (<    >)
+	$partOfDay = "";
+	if ( $hourNow < 16 ){
+		$partOfDay = "varajane hommik";
+		
+	//echo $partOfDay
+	if ( $hourNow >= 8 and $hourNow <16 ) (
+		$partOfDay = "koolipäev";
+	
+	if ( $hourNow >= 16) (
+		$partOfDay = "vaba aeg"; (
+	}
+	?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,12 +27,16 @@
 	</title>
 </head>
 <body>
-	<hl> Sten </hl>
+	<h1>
+	<?php
+	echo $myName ." " .$myFamilyName;
+	?>
+	Sten </h1>
 	<p>See veebileht on loodud õppetöö raames ning ei sisalda tõsiseltvõetavat sisu.</p>
 	<?php
 	echo "Kõige esimene php abil väljastatud sõnum";
 	echo "<p>Täna on ";
-	echo date("d.m.y");
+	echo date("d.m.y");.".$partOfDay;
 	echo ".<p>";
 	echo "<p>Lehe avamise hetkel oli kell " .date ("h:i:s") .".</p>";
 	?>
