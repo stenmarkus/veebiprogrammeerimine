e15e6fdb3d6e3dd10d8b177afaaa56a74844a695
Sten Markus Laht <stenmarkus@usersnoreply.github.com>
2017-09-11 13:48:48 +0300

    esimene PHP fail, kus on vaid html

---
+++ esimene.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+       programmeerimise asjad
+	</title>
+</head>
+<body>
+	<hl>Sten</hl>
+	<p>See veebileht on loodud õppetöö raames ning ei sisalda tõsiseltvõetavat sisu.</p>
+</body>
+</html>
