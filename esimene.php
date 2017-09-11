42f5b650d4532fd4a7de4f645c29e0aa9f04c3d2
Sten Markus Laht <stenmarkus@usersnoreply.github.com>
2017-09-11 15:33:23 +0300

    esimene kood

--- esimene.php
+++ esimene.php
@@ -7,7 +7,14 @@
 	</title>
 </head>
 <body>
-	<hl>Sten</hl>
+	<hl> Sten </hl>
 	<p>See veebileht on loodud õppetöö raames ning ei sisalda tõsiseltvõetavat sisu.</p>
+	<?php
+	echo "Kõige esimene php abil väljastatud sõnum";
+	echo "<p>Täna on ";
+	echo date("d.m.y");
+	echo ".<p>";
+	echo "<p>Lehe avamise hetkel oli kell " .date ("h:i:s") .".</p>";
+	?>
 </body>
 </html>
