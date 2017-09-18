17f5e56e3fdb37f8bfd96cc0822abedca86f89f8
Sten Markus Laht <stenmarkus@usersnoreply.github.com>
2017-09-18 14:35:03 +0300

    lisatud vorm ja tsükkel

--- esimene.php
+++ esimene.php
@@ -17,10 +17,28 @@
 	if ( $hourNow >= 16) (
 		$partOfDay = "vaba aeg"; (
 	}
+	//vanusega seotud muutujad
+	$myAge=0;
+	$ageNote = "";
+	$myBirthYear;
+	
+	
+	//echo $_POST;
+	//arvutame vanuse
+	if ( isset)($_POST("birthyear"]))(
+	$myAge= date("Y") - $_POST_("birthyear");
+	$ageNote = "<p>Te olete umbes" . $myAge. "aastat vana<p>"
+	}
+	//lihtne tsükkel
+	for ($i = 0; $i <5; $i ++) {
+		echo "ha";	
+	}	
+	
 	?>
 <!DOCTYPE html>
 <html>
 <head>
+
 	<meta charset="utf-8">
 	<title>
        programmeerimise asjad
@@ -34,11 +52,29 @@
 	Sten </h1>
 	<p>See veebileht on loodud õppetöö raames ning ei sisalda tõsiseltvõetavat sisu.</p>
 	<?php
+	
+	
 	echo "Kõige esimene php abil väljastatud sõnum";
 	echo "<p>Täna on ";
-	echo date("d.m.y");.".$partOfDay;
+	echo date("d.m.y").$partOfDay;
+	
 	echo ".<p>";
 	echo "<p>Lehe avamise hetkel oli kell " .date ("h:i:s") .".</p>";
 	?>
+	<form method= "POST">
+		<label>1998: </label>
+		<input name ="birthyear" id= "birthyear" type= "number" max= "2017" min= "1900">
+	<input id= "submitbirthyear" type="submit" value= "Kinnita">
+	</form>
+	<?php
+		if( $ageNote != "")(
+			echo $ageNote
 </body>
 </html>
+	
+	
+
+
+
+	
+
