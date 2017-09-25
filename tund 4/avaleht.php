b58955af23bb075f797167908529d453169351bc
Sten Markus Laht <stenmarkus@usersnoreply.github.com>
2017-09-25 12:58:27 +0300

    suvalised pildid

---
+++ tund 4/avaleht.php
@@ -0,0 +1,29 @@
+	
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="utf-8">
+	<title>
+       programmeerimise asjad
+	</title>
+</head>
+<body>
+	<h1>
+	
+	Sten </h1>
+	<p>See veebileht on loodud õppetöö raames ning ei sisalda tõsiseltvõetavat sisu.</p>
+	<h2>Õpime<a href ="http://www.tlu.ee"> Tallinna Ülikoolis</a></p><h2>
+	<p>Minu esimene php-leht on<a href="../esimene.php"> siin</a></p>
+	<p> Minu sõbra Joonase leht on <a href= "../../../~masijoon/veebiprogrammeerimine">siin</a></p>
+	<p>Pilte ülikoolist näeb <a href= "foto.php"> siin</a></p/>
+	
+</body>
+</html>
+	
+	
+
+
+
+	
+
